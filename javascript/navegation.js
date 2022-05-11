ddf40ba605536730c2ee6358851626fff82ffb3b
Matheus Palmieri <matheusandrepp@gmail.com>
2022-05-11 13:14:11 -0300

    Correções

---
+++ javascript/navegation.js
@@ -0,0 +1,4 @@
+// var inicioNav = document.getElementById("inicio")
+// var skillsNav = document.getElementById("skills")
+// var projetosNav = document.getElementById("projetos")
+// var contatoNav = document.getElementById("contato")
